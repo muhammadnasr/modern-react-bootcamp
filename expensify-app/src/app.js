691bce9444f0fc0f67fa0f22421e20f33bd6d358
Muhammad Hassan Nasr <muhammad.hassan.nasr@gmail.com>
2022-07-28 16:33:29 +0300

    add todo

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -8,6 +8,7 @@ import store from './store/configureStore';
 import { addExpense } from './reducers/expenses';
 import Expense from './models/Expense';
 
+//todo implement testing for redux and components using RTL
 store.dispatch(addExpense(new Expense({ description: 'Water bill', amount: 4500 }).serialize()))
 store.dispatch(addExpense(new Expense({ description: 'Gas bill', createdAt: 1000 }).serialize()))
 store.dispatch(addExpense(new Expense({ description: 'Rent', amount: 109500 }).serialize()))
